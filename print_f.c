5f4adb0ef949e939b25dffab5aa6e08c19f6faac
Kojo Antwi Osei-Sarfo <astrokojo@gmail.com>
2023-08-22 11:05:46 -0700

    Hopeful

--- print_f.c
+++ print_f.c
@@ -71,7 +71,6 @@ int _printf(const char *format, ...)
 
 				print_string(str, &char_print);
 			}
-/*both "d" and "i" output as integers*/
 			else if (*format == 'd' || *format == 'i')
 			{
 				int n = va_arg(arg, int);
